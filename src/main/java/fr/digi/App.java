c257a849438c53ff4293af3cf95b2449ab08e5fc
Votre Nom <h.hassen.malek@gmail.com>
2024-11-05 10:12:06 +0100

    find livre id

--- src/main/java/fr/digi/App.java
+++ src/main/java/fr/digi/App.java
@@ -12,34 +12,38 @@ public class App {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
-        // CRUD ==>Creste/Insert
-       /* Livre livreACreer = new Livre (6 ,"Harry Potter" ,  "J.K. Rowling");*/
-        Livre livreACreer= new Livre ("Harry Potter" ,  "J.K. Rowling");
-        em.persist(livreACreer);
-        em.persist(livreACreer);
-        //update Silencieux;
-        livreACreer.setAuteur("Hassen MALEK");
+//        // CRUD ==>Creste/Insert
+//       /* Livre livreACreer = new Livre (6 ,"Harry Potter" ,  "J.K. Rowling");*/
+//        Livre livreACreer= new Livre ("Harry Potter" ,  "J.K. Rowling");
+//        em.persist(livreACreer);
+//        em.persist(livreACreer);
+//        //update Silencieux;
+//        livreACreer.setAuteur("Hassen MALEK");
 
-        // CRUD ==>Read/Select
-        Livre livreALire = em.find(Livre.class, 8); //il faut un id  pour le find fonctionne
-        if(livreALire != null){
-            System.out.println(livreALire.getTitre());
-        }
+//        // CRUD ==>Read/Select
+//        Livre livreALire = em.find(Livre.class, 8); //il faut un id  pour le find fonctionne
+//        if(livreALire != null){
+//            System.out.println(livreALire.getTitre());
+//        }
 
-        //CRUD Update/Modifier
-        Livre livreAModifier = em.find(Livre.class, 9);
-        if(livreALire != null){
-            livreAModifier.setTitre("Les Misérable");
-        }
+//        //CRUD Update/Modifier
+//        Livre livreAModifier = em.find(Livre.class, 9);
+//        if(livreALire != null){
+//            livreAModifier.setTitre("Les Misérable");
+//        }
 
-        //CRUD Delete
-        Livre livreASupprimer = em.find(Livre.class, 8);
-        if(livreASupprimer != null ){
-            em.remove(livreASupprimer);
-        }
+//        //CRUD Delete
+//        Livre livreASupprimer = em.find(Livre.class, 8);
+//        if(livreASupprimer != null ){
+//            em.remove(livreASupprimer);
+//        }
+
+
+
+        Livre livre = em.find(Livre.class, 5);
 
         em.getTransaction().commit();
-        System.out.println(livreACreer);
+        System.out.println(livre);
         em.close();
         emf.close();
     }
